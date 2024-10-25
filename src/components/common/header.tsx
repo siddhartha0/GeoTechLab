f3eb12c57c77e8109f1423a10c07b06b74dcba8a
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-10-25 22:20:06 +0545

    refactored

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,15 +1,7 @@
 import { Link } from "react-router-dom";
 import { Text } from "../units";
 import logo from "../../assets/images/logo.png";
-const HeaderData = [
-  { id: 1, title: "home", path: "" },
-  { id: 2, title: "our team", path: "" },
-  { id: 3, title: "capabilities", path: "" },
-  { id: 4, title: "Projects", path: "" },
-  { id: 5, title: "Affiliation & Membership", path: "" },
-  { id: 6, title: "Request Testing", path: "" },
-  { id: 7, title: "Contact Us", path: "" },
-];
+import { HeaderData } from "../../utils/constants/HeaderData";
 
 export const Header = () => {
   return (
---
+++ src/utils/constants/HeaderData.ts
@@ -0,0 +1,9 @@
+export const HeaderData = [
+  { id: 1, title: "home", path: "" },
+  { id: 2, title: "our team", path: "" },
+  { id: 3, title: "capabilities", path: "" },
+  { id: 4, title: "Projects", path: "" },
+  { id: 5, title: "Affiliation & Membership", path: "" },
+  { id: 6, title: "Request Testing", path: "" },
+  { id: 7, title: "Contact Us", path: "" },
+];
