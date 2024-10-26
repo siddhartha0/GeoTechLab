72059a9e194bf08fee3a54704e3c34835834bbff
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-10-26 11:31:36 +0545

    core init

---
+++ src/components/pages/home/core.tsx
@@ -0,0 +1,3 @@
+export const Core = () => {
+  return <div></div>;
+};
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,13 @@
 import { LandingPageLayout } from "../../components/common/landingpagelayout";
 import { Banner } from "../../components/pages/home/banner";
+import { Core } from "../../components/pages/home/core";
 
 export const Home = () => {
   return (
     <LandingPageLayout>
       <div className="flex flex-col ">
         <Banner />
+        <Core />
       </div>
     </LandingPageLayout>
   );
