6bfa8103cb441673c8989d173145af73b8d74466
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-10-28 19:04:54 +0545

    slider added

--- src/components/pages/home/banner.tsx
+++ src/components/pages/home/banner.tsx
@@ -1,12 +1,28 @@
 import { Text } from "../../units";
 import { Button } from "../../units/Button";
 import slider from "../../../assets/images/bg.jpg";
+import banner1 from "../../../assets/images/banner1.jpeg";
+import banner2 from "../../../assets/images/banner2.jpeg";
+import banner3 from "../../../assets/images/banner3.jpeg";
+import { useEffect, useState } from "react";
 
 export const Banner = () => {
+  const images = [slider, banner1, banner2, banner3];
+  const [bannerImage, setBannerImage] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setBannerImage((prevIndex) => (prevIndex + 1) % images.length);
+    }, 10000);
+
+    return () => clearInterval(intervalId); // Cleanup on unmount
+  }, []);
+
+  console.log("rendereds");
   return (
     <div
       className="flex bg-cover bg-center font-sans"
-      style={{ backgroundImage: `url(${slider})` }}
+      style={{ backgroundImage: `url(${images[bannerImage]})` }}
     >
       <section className="flex flex-col gap-20 w-[675px] px-8 py-28 font-sans">
         <Text size="heading-lg-default" secondaryFont>
